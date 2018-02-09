f8958f17cb8075b04516f257ff786d4647bfed68
Jack LeFevre <jackflef@gmail.com>
2018-02-09 13:08:37 -0500

    Added documentation to abstract interfaces for commands.

--- src/commands/interfaces/Command.java
+++ src/commands/interfaces/Command.java
@@ -1,41 +1,75 @@
 package commands.interfaces;
 
 /**
- * abstract interface for a command
+ * abstract interface for a command.
  * 
  * @author jackf
  *
  */
 public abstract class Command {
-	//the callback to call upon ending
+	/**
+	 * the callback to call upon ending
+	 */
 	private CommandCallback callback;
-	//the readable name of the command
+	/**
+	 * the readable name of the command
+	 */
 	protected String readableName = "Unnamed command";
-	public Command(CommandCallback callback, String name){
-		this(callback);
+
+	/**
+	 * Constructor for a command. Takes a callback which is to notify the
+	 * opmode/robot that the command is finished, and a readable name for the
+	 * command.
+	 * 
+	 * @param callback
+	 *            the opmode/robot that the command is run by
+	 * @param name
+	 *            The name of the command, should be a descriptive/readable string.
+	 */
+	public Command(CommandCallback callback, String name) {
+		this.callback = callback;
 		readableName = name;
 	}
-	public Command(CommandCallback callback){
-		this.callback = callback;
+
+	/**
+	 * called once when the command starts.
+	 */
+	public void init() {
 	}
-	//called once
-	public void init(){}
-	//called periodically
-	public void periodic(double deltaTime){}
-	//called once or never, to stop the command
-	public void stop(){}
-	
-	//called when the command ends and calls back
-	protected void endCommand(){
+
+	/**
+	 * called periodically when the command is run.
+	 * 
+	 * @param deltaTime
+	 *            the amount of time passed since the call of periodic or init.
+	 */
+	public void periodic(double deltaTime) {
+	}
+
+	/**
+	 * called once or never, to stop the command.
+	 */
+	public void stop() {
+	}
+
+	/**
+	 * called when the command ends and calls back. This is used to end the command
+	 * before stop is called.
+	 */
+	protected void endCommand() {
 		callback.commandFinished(this);
 	}
-	
+	/**
+	 * get the readable name of the command. Identical to toString().
+	 * @return the readable name of the command
+	 */
 	public String getName() {
 		return readableName;
 	}
-	//toString used for printing
+
+	// toString used for printing
 	@Override
-	public String toString(){
+	public String toString() {
 		return readableName;
 	}
 }
--- src/commands/interfaces/OpMode.java
+++ src/commands/interfaces/OpMode.java
@@ -10,43 +10,83 @@ import org.usfirst.frc.team3641.robot.Robot;
  * @author jackf
  *
  */
-public abstract class OpMode extends Command implements CommandCallback{
+public abstract class OpMode extends Command implements CommandCallback {
+	/**
+	 * A list of running commands. On periodic, all commands are updated.
+	 */
 	private ArrayList<Command> commands;
+	/**
+	 * The robot that the command is running on.
+	 */
 	protected Robot robot;
-	public OpMode(Robot bot, String name){
+
+	/**
+	 * Constructor for a defined name.
+	 * 
+	 * @param bot
+	 *            The robot the command is running in
+	 * @param name
+	 *            the name of the opmode.
+	 */
+	public OpMode(Robot bot, String name) {
 		super(bot, name);
 		commands = new ArrayList<Command>();
 		robot = bot;
 	}
-	
-	public OpMode(Robot bot){
-		super(bot);
+
+	/**
+	 * Constructor using no defined name. Avoid this.
+	 * 
+	 * @param bot
+	 *            the robot the opmode is running on
+	 */
+	public OpMode(Robot bot) {
+		super(bot, "Default opmode name");
 		commands = new ArrayList<Command>();
 		robot = bot;
 	}
-	//called once
-	public void init(){
-		//No initing of commands, since they may not start immediately.
+
+	/**
+	 * called once when the command is started (during firstPeriodic in Robot)
+	 */
+	public void init() {
+		// No auto-initing of commands, since they may not start immediately.
 	}
-	//called periodically
-	public void periodic(double deltaTime){
-		for(Command cmd:commands){
+
+	/**
+	 * called periodically during teleop/autonomous periodic.
+	 */
+	public void periodic(double deltaTime) {
+		for (Command cmd : commands) {
 			cmd.periodic(deltaTime);
 		}
 	}
-	//called once or never, to stop the command
-	public void stop(){
-		for(Command cmd:commands){
+
+	/**
+	 * called once or never, to stop the opmode.
+	 */
+	public void stop() {
+		for (Command cmd : commands) {
 			cmd.stop();
 		}
 	}
-	
+
+	/**
+	 * Add a new command to the list of commands
+	 * 
+	 * @param cmd
+	 *            the command to add
+	 */
 	protected void addCommand(Command cmd) {
 		commands.add(cmd);
 		cmd.init();
 	}
-	
-	public void commandFinished(Command cmd){
+
+	/**
+	 * Callback for commands that finish. Removes the command from the list of
+	 * commands.
+	 */
+	public void commandFinished(Command cmd) {
 		commands.remove(cmd);
 	}
 }
