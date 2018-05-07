8b8058eed502d9f5570884a7654bf39ecd9e153b
school19 <school19@msu.edu>
2018-05-07 13:39:00 -0400

    Added support for automated testing and an example test class. The test is set to automatically fail.

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldTest
+{
+    @Test
+    public void helloWorld() 
+    {
+       Assert.assertTrue(false);
+       System.out.println("Hello World!");
+    } 
+}
